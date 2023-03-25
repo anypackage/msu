9ad2288a4ab326c6d0041d93333fdb80b4ff7c71
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-25 15:48:13 -0500

    Add Get-Package support (#1)

--- src/code/MsuProvider.cs
+++ src/code/MsuProvider.cs
@@ -3,14 +3,30 @@
 // terms of the MIT license.
 
 using System;
+using System.Management;
 
 namespace AnyPackage.Provider.Msu
 {
     [PackageProvider("Msu")]
-    public sealed class MsuProvider : PackageProvider
+    public sealed class MsuProvider : PackageProvider, IGetPackage
     {
         private readonly static Guid s_id = new Guid("314633fe-c7e9-4eeb-824b-382a8a4e92b8");
 
         public MsuProvider() : base(s_id) { }
+
+        public void GetPackage(PackageRequest request)
+        {
+            var quickFix = new ManagementObjectSearcher(@"root\cimv2", "select * from Win32_QuickFixEngineering");
+
+            foreach (var hotFix in quickFix.Get())
+            {
+                if (request.IsMatch((string)hotFix["HotFixID"]))
+                {
+                    request.WritePackage((string)hotFix["HotFixID"],
+                                         new PackageVersion("0"),
+                                         (string)hotFix["Description"]);
+                }
+            }
+        }
     }
 }
